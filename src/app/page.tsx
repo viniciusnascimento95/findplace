c9ef514597b69668ba244547df9269073d53e06b
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-10-17 21:50:17 -0300

    WIP: atualizando form para validações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 
-
 interface Values {
   eventName: string
   description: string
-  startDate: string
-  endDate: string
+  startDate: Date
+  endDate: Date
   address: string
   number: string
   neighborhood: string
@@ -17,13 +16,23 @@ interface Values {
   postalCode: string
   capacity: string
   eventType: string
-  accessibility: boolean
+  accessibility: string
   organizerName: string
   organizerEmail: string
   organizerPhone: string
 }
 
+interface ErrorMessageProps {
+  touched: boolean | undefined;
+  error: string | undefined;
+}
+
+
 export default function Home() {
+
+  const ErrorMessage = ({ touched, error }: ErrorMessageProps) => (
+    touched && error ? <div className="text-red-500 text-sm">{error}</div> : null
+  );
 
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -35,7 +44,7 @@ export default function Home() {
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Everything you need to deploy your app
 
-          
+
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Formulário <br />
@@ -48,8 +57,8 @@ export default function Home() {
             initialValues={{
               eventName: '',
               description: '',
-              startDate: '',
-              endDate: '',
+              startDate: new Date(),
+              endDate: new Date(),
               address: '',
               number: '',
               neighborhood: '',
@@ -58,7 +67,7 @@ export default function Home() {
               postalCode: '',
               capacity: '',
               eventType: '',
-              accessibility: false,
+              accessibility: '',
               organizerName: '',
               organizerEmail: '',
               organizerPhone: '',
@@ -92,11 +101,9 @@ export default function Home() {
               }, 500);
             }}
           >
-            {({ isSubmitting, touched, errors,values, handleBlur }) => (
+            {({ isSubmitting, touched, errors, values, handleBlur, handleChange }) => (
               <Form className="max-w-lg p-4 bg-white shadow-md rounded-lg">
-                <p className="block text-gray-700">
-                  
-                  {JSON.stringify(errors, null, 3)}</p>
+                <p className="text-gray-700">{JSON.stringify(errors, null, 2)}</p>
                 <div className="mb-4 text-left">
                   <label htmlFor="eventName" className="block text-gray-700">Nome do Evento</label>
                   <p className="block text-gray-700"> {JSON.stringify(values)}</p>
@@ -117,6 +124,7 @@ export default function Home() {
                     as="textarea"
                     id="description"
                     name="description"
+                    onBlur={handleBlur}
                     className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
                   />
                   {touched.description && errors.description && (
@@ -124,88 +132,183 @@ export default function Home() {
                   )}
                 </div>
 
-                {/* <div className="mb-4 text-left">
-                  <label htmlFor="description" className="block text-gray-700">Descrição do Evento</label>
-                  <textarea
-                    id="description"
-                    name="description"
-                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.description}
-                  />
-                  {formik.touched.description && formik.errors.description ? (
-                    <div className="text-red-500 text-sm">{formik.errors.description}</div>
-                  ) : null}
-                </div> */}
+                <div className='flex flex-row'>
+                  <div className="mb-4 text-left basis-1/4 mr-3">
+                    <label htmlFor="startDate" className="block text-gray-700">Data de Início</label>
+                    <Field
+                      id="startDate"
+                      name="startDate"
+                      type="date"
+                      className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    />
+                    <ErrorMessage name="startDate" component="div" className="text-red-500 text-sm" />
+                  </div>
 
-                {/* <div className="mb-4 text-left">
-                  <label htmlFor="startDate" className="block text-gray-700">Data de Início</label>
+                  <div className="mb-4 text-left basis-1/4">
+                    <label htmlFor="endDate" className="block text-gray-700">Data de Término</label>
+                    <Field
+                      id="endDate"
+                      name="endDate"
+                      type="date"
+                      className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    />
+                    <ErrorMessage name="endDate" component="div" className="text-red-500 text-sm" />
+                  </div>
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="postalCode" className="block text-gray-700">CEP</label>
                   <input
-                    id="startDate"
-                    name="startDate"
-                    type="date"
+                    id="postalCode"
+                    name="postalCode"
+                    type="text"
                     className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.startDate}
                   />
-                  {formik.touched.startDate && formik.errors.startDate ? (
-                    <div className="text-red-500 text-sm">{formik.errors.startDate}</div>
+                  {touched.postalCode && errors.postalCode ? (
+                    <div className="text-red-500 text-sm">{errors.postalCode}</div>
                   ) : null}
-                </div> */}
+                </div>
 
-                {/* <div className="mb-4 text-left">
-                  <label htmlFor="endDate" className="block text-gray-700">Data de Término</label>
-                  <input
-                    id="endDate"
-                    name="endDate"
-                    type="date"
-                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.endDate}
-                  />
-                  {formik.touched.endDate && formik.errors.endDate ? (
-                    <div className="text-red-500 text-sm">{formik.errors.endDate}</div>
-                  ) : null}
-                </div> */}
-
-                {/* <div className="mb-4 text-left">
+                <div className="mb-4 text-left">
                   <label htmlFor="address" className="block text-gray-700">Endereço</label>
                   <input
                     id="address"
                     name="address"
                     type="text"
                     className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.address}
-                  />
-                  {formik.touched.address && formik.errors.address ? (
-                    <div className="text-red-500 text-sm">{formik.errors.address}</div>
-                  ) : null}
-                </div> */}
 
-                {/* <div className="mb-4 text-left">
+                  />
+                  {touched.address && errors.address ? (
+                    <div className="text-red-500 text-sm">{errors.address}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
                   <label htmlFor="city" className="block text-gray-700">Cidade</label>
                   <input
                     id="city"
                     name="city"
                     type="text"
                     className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.city}
                   />
-                  {formik.touched.city && formik.errors.city ? (
-                    <div className="text-red-500 text-sm">{formik.errors.city}</div>
+                  {touched.city && errors.city ? (
+                    <div className="text-red-500 text-sm">{errors.city}</div>
                   ) : null}
-                </div> */}
+                </div>
 
-                <button type="submit" className="mt-4 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">
-                  Salvar
-                </button>
+                <div className="mb-4 text-left">
+                  <label htmlFor="state" className="block text-gray-700">Estado</label>
+                  <input
+                    id="state"
+                    name="state"
+                    type="text"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                  />
+                  {touched.state && errors.state ? (
+                    <div className="text-red-500 text-sm">{errors.state}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="capacity" className="block text-gray-700">Capacidade de pessoas</label>
+                  <input
+                    id="capacity"
+                    name="capacity"
+                    type="number"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                  />
+                  {touched.capacity && errors.capacity ? (
+                    <div className="text-red-500 text-sm">{errors.capacity}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="eventType" className="block text-gray-700">Tipo de Evento</label>
+                  <input
+                    id="eventType"
+                    name="eventType"
+                    type="number"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                  />
+                  {touched.eventType && errors.eventType ? (
+                    <div className="text-red-500 text-sm">{errors.eventType}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label className="block text-gray-700 mb-2">O local é acessível?</label>
+                  <div className="flex items-center space-x-4">
+                    <label className="inline-flex items-center">
+                      <input
+                        type="radio"
+                        name="accessibility"
+                        value="SIM"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        checked={values.accessibility === 'SIM'}
+                        className="form-radio text-blue-600 focus:ring-blue-500"
+                      />
+                      <span className="ml-2 text-gray-700">Sim</span>
+                    </label>
+                    <label className="inline-flex items-center">
+                      <input
+                        type="radio"
+                        name="accessibility"
+                        value="NAO"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        checked={values.accessibility === 'NAO'}
+                        className="form-radio text-blue-600 focus:ring-blue-500"
+                      />
+                      <span className="ml-2 text-gray-700">Não</span>
+                    </label>
+                  </div>
+                  {touched.accessibility && errors.accessibility ? (
+                    <div className="text-red-500 text-sm">{errors.accessibility}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="organizerName" className="block text-gray-700">Organizadora</label>
+                  <input
+                    id="organizerName"
+                    name="organizerName"
+                    type="text"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+
+                  />
+                  {touched.organizerName && errors.organizerName ? (
+                    <div className="text-red-500 text-sm">{errors.organizerName}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="organizerEmail" className="block text-gray-700">E-mail Organizadora</label>
+                  <input
+                    id="organizerEmail"
+                    name="organizerEmail"
+                    type="email"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+
+                  />
+                  {touched.organizerEmail && errors.organizerEmail ? (
+                    <div className="text-red-500 text-sm">{errors.organizerEmail}</div>
+                  ) : null}
+                </div>
+
+                <div className="mb-4 text-left">
+                  <label htmlFor="organizerPhone" className="block text-gray-700">Contato Organizadora</label>
+                  <input
+                    id="organizerPhone"
+                    name="organizerPhone"
+                    type="tel"
+                    className="mt-1 w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+
+                  />
+                  {touched.organizerPhone && errors.organizerPhone ? (
+                    <div className="text-red-500 text-sm">{errors.organizerPhone}</div>
+                  ) : null}
+                </div>
 
                 <button
                   type="submit"
